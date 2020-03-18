56d988141d2cb435d8a4200e569f4b9d3ec7a677
Rebecca Vest <rvest@healthline.com>
2020-03-18 15:51:06 -0600

    Apiary embed

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/:name', function(req, res) {
+  res.render('index', { name:  req.params.name });
+});
+
+module.exports = router;
