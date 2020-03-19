ab8c182f800c43479b281367b9392346a1549294
Rebecca Vest <rvest@healthline.com>
2020-03-18 22:12:34 -0600

    Add Github

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const getContents = require('../lib/github');
+
+require('dotenv').config();
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/:name', function(req, res) {
-  res.render('index', { name:  req.params.name });
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/apiary/:name', (req, res) => {
+  res.render('index', { name: req.params.name });
+});
+
+router.get('/apiary/:name/embed', (req, res) => {
+  res.json({
+    version: '1.0',
+    type: 'rich',
+    provider_name: 'Apiary',
+    provider_url: 'http://www.apiary.io/',
+    width: 425,
+    height: 344,
+    title: '',
+    author_name: 'Rebecca Vest',
+    author_url: 'http://www.github.com/idahogurl',
+    html: `<iframe src="https://apiary-embed.herokuapp.com/apiary/${req.params.name}"></iframe>`,
+  });
+});
+
+router.get('/github', async (req, res) => {
+  const content = getContents(req.query.permalink);
+  res.render('github', content);
+});
+
+router.get('/github/embed', (req, res) => {
+  res.json({
+    version: '1.0',
+    type: 'rich',
+    provider_name: 'GitHub',
+    provider_url: 'http://www.github.com/',
+    width: 425,
+    height: 344,
+    title: '',
+    author_name: 'Rebecca Vest',
+    author_url: 'http://www.github.com/idahogurl',
+    html: `<iframe src="https://apiary-embed.herokuapp.com/github/?permalink=${req.query.permalink}"></iframe>`,
+  });
 });
 
 module.exports = router;
